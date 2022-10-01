405fd910b58dea69e26921ec294de145976a78e1
elkingonzalezcuellar <elkin.gonzalez-c@mail.escuelaing.edu.co>
2022-10-01 00:25:41 -0500

    Menu y carrito

--- main.js
+++ main.js
@@ -4,12 +4,38 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuMobileLogo = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+const carritoComprasIcon = document.querySelector(".navbar-shopping-cart");
+const carritoCompras = document.querySelector(".product-detail");
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuMobileLogo.addEventListener("click", toggleMobileMenu);
+carritoComprasIcon.addEventListener("click", toggleCarritoCompras);
 
 function toggleDesktopMenu() {
+  const isCarritoClosed = carritoCompras.classList.contains("inactive");
+  if (!isCarritoClosed) {
+    carritoCompras.classList.add("inactive");
+  }
   desktopMenu.classList.toggle("inactive");
 }
+
 function toggleMobileMenu() {
+  const isCarritoClosed = carritoCompras.classList.contains("inactive");
+  if (!isCarritoClosed) {
+    carritoCompras.classList.add("inactive");
+  }
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoCompras() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  const isMenuDesktopClosed = desktopMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+  if (!isMenuDesktopClosed) {
+    desktopMenu.classList.add("inactive");
+  }
+  carritoCompras.classList.toggle("inactive");
 }
